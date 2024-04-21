bccb4f23c522b3f33f7ad1c31f4103723de2c7ee
tediko <tediko123@wp.pl>
2024-04-21 12:07:05 +0200

    Changed debounce function, added selectors and use dataset on link instead of cropping href attribute

--- src/smoothScroll.js
+++ src/smoothScroll.js
@@ -2,43 +2,46 @@ import { gsap } from "gsap";
 import { ScrollToPlugin } from "gsap/ScrollToPlugin";
 gsap.registerPlugin(ScrollToPlugin);
 
-// Get a list of all the sections
-let sections = gsap.utils.toArray(".panel");
+// Selectors
+const sectionContainers = [...document.querySelectorAll('.panel')];
+const linkElements = [...document.querySelectorAll('[data-link]')];
+
+// Flags
+let sectionTops;
 
 // Calculate the top position of each section relative to the viewport
-let sectionTops = sections.map(section => ({
-  id: section.id,
-  top: section.getBoundingClientRect().top
-}));
-
-// Function to update the sectionTops array on window resize (debounced)
+// and update the sectionTops array on window resize (debounced) or initial load
 const updateSectionTops = () => {
   window.scrollTo(0, 0);
-  sectionTops = sections.map(section => ({
+
+  sectionTops = sectionContainers.map(section => ({
     id: section.id,
     top: section.getBoundingClientRect().top
   }));
 }
 
 // Debounce the updateSectionTops function
-const debounce = (func, wait) => {
-  let timeout;
-  return function(...args) {
-    const context = this;
-    clearTimeout(timeout);
-    timeout = setTimeout(() => func.apply(context, args), wait);
+const debounce = (mainFunction, delay) => {
+  let timer;
+  
+  return function (...args) {
+    clearTimeout(timer);
+    
+    timer = setTimeout(() => {
+      mainFunction(...args);
+    }, delay);
   };
-}
+};
 
 // Call updateSectionTops() immediately to initialize sectionTops
 updateSectionTops();
 
 // Add a click event listener to each navigation link
-export default gsap.utils.toArray("[data-link]").forEach(link => {
+export default linkElements.forEach(link => {
   link.addEventListener("click", (event) => {
     event.preventDefault();
-    const targetId = link.getAttribute('href').slice(1);
-    const targetSection = sectionTops.find(section => section.id === targetId);
+    const targetName = link.dataset.link;
+    const targetSection = sectionTops.find(section => section.id === targetName);
     gsap.to(window, { duration: 2, scrollTo: targetSection.top });
   });
 });
